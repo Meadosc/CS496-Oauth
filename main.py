d33cbbd0278b4972eb4d52ed773d2474557b846a
Cord Meados <meadosc@gmail.com>
2017-10-22 19:18:27 -0700

    Fifth commit

--- main.py
+++ main.py
@@ -11,17 +11,18 @@ import urllib
 import random
 import json
 
-
+#Constatants
 CLIENT_ID = '362935595119-o0grnh36hbunrrfvakuh0duoi8clpv47.apps.googleusercontent.com'
 CLIENT_SECRET = 'ASdcpzNg3AM1zhR0hi0sskTR'
 REDIRECT_URI = 'https://cs496-oauth-183823.appspot.com/oauth'
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+	    #Create random string for authentication
         random_string = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
 
         url_linktext = 'Provide Access'
-
+        #build URL for google request and direct user
         url = "https://accounts.google.com/o/oauth2/v2/auth?"
         url = url + "scope=email"
         url = url + "&access_type=offline"
@@ -49,11 +50,11 @@ class OAuthHandler(webapp2.RequestHandler):
             'grant_type': 'authorization_code'
             }
 
-        payload = urllib.urlencode(post_body)
+        pload = urllib.urlencode(post_body)
         headers = {'Content-Type':'application/x-www-form-urlencoded'}
         result = urlfetch.fetch(
             url="https://www.googleapis.com/oauth2/v4/token",
-   		 	payload = payload,
+   		 	payload = pload,
     		method = urlfetch.POST,
     		headers = headers)
 
@@ -65,33 +66,33 @@ class OAuthHandler(webapp2.RequestHandler):
             method = urlfetch.GET,
             headers=headers)
 
+		#check that all data is there, else send "noAccount message"
         json_result = json.loads(result.content)
-        # self.response.write(json.dumps(json_result['name']['givenName']))
         exist_fname = False
         exist_lname = False
         exist_gplink = False
-        exist_account = False
-        for item in json_result:
-            if item == 'name':
-                if item[0]:
+        existaccount = False
+        for x in json_result:
+            if x == 'name':
+                if x[0]:
                     exist_fname = True
-                if item[0]:
+                if x[0]:
                     exist_lname = True
-            if item == 'url':
+            if x == 'url':
                 exist_gplink = True
 
-        if exist_fname and exist_lname and exist_gplink:
-            fname = json_result['name']['givenName']
-            lname = json_result['name']['familyName']
+        if (exist_fname and exist_lname and exist_gplink): #if all is well, list values
+            first_name = json_result['name']['givenName']
+            last_name = json_result['name']['familyName']
             gplink = str(json_result['url'])
-            exist_account = True
-            template_values = {'fname': fname,
-                               'lname': lname,
+            existaccount = True
+            template_values = {'fname': first_name,
+                               'lname': last_name,
                                'gplink': gplink,
                                'gplink_name': "Visit Profile",
                                'state': state}
         else:
-            template_values = {'noAccount': "NOTE: No Google+ account found", 'state': state}
+            template_values = {'noAccount': "You do not have a Google+ account", 'state': state}
 
         path = os.path.join(os.path.dirname(__file__), 'templates/oauth.html')
         self.response.out.write(template.render(path, template_values))
